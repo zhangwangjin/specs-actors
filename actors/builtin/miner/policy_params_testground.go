7f9384acdba05cd871a5e480a3c072e536e17638
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-07-10 16:35:39 +0200

    more aggressive down-scale

--- actors/builtin/miner/policy_params_testground.go
+++ actors/builtin/miner/policy_params_testground.go
@@ -7,13 +7,13 @@ import (
 )
 
 // The period over which all a miner's active sectors will be challenged.
-const WPoStProvingPeriod = abi.ChainEpoch(360) // proving period set to 360 epochs ~ 6min. instead of 24 hours (assuming block time delay of 1sec.)
+const WPoStProvingPeriod = abi.ChainEpoch(72) // proving period set to 36*3 epochs ~ 6min. instead of 24 hours (assuming block time delay of 1sec.)
 
 // The duration of a deadline's challenge window, the period before a deadline when the challenge is available.
-const WPoStChallengeWindow = abi.ChainEpoch(10) // challenge window set to 10 epochs ~ 10sec. instead of 40 minutes (assuming block time delay of 1sec.)
+const WPoStChallengeWindow = abi.ChainEpoch(2) // challenge window set to 10 epochs ~ 10sec. instead of 40 minutes (assuming block time delay of 1sec.)
 
 // The number of non-overlapping PoSt deadlines in each proving period.
 const WPoStPeriodDeadlines = uint64(WPoStProvingPeriod / WPoStChallengeWindow) // 30 periods (one period every 10 epochs?)
 
 // The maximum age of a fault before the sector is terminated.
-const FaultMaxAge = WPoStProvingPeriod*14 - 1
+const FaultMaxAge = WPoStProvingPeriod*3 - 1 // not 14 days, but 3 days
